5059bc53a0eb838732df4e015cb04bf2702ea9a9
IcaroCaetano <icaro.caetano@gmail.com>
2023-06-20 23:08:35 -0300

    add Botao

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,6 +1,7 @@
 import './Formulario.css'
 import CampoTexto from '../CampoTexto';
 import ListaSuspensa from '../ListaSuspensa';
+import Botao from '../Botao';
 
 const Formulario = () => {
     const times = [
@@ -21,6 +22,7 @@ const Formulario = () => {
                 <CampoTexto label="Cargo" placeholder="Digite o seu Cargo"/>
                 <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem"/>
                 <ListaSuspensa label="Time" itens={times}></ListaSuspensa>
+                <Botao texto="Criar Card">/</Botao>
             </form>
         </section>
     )
