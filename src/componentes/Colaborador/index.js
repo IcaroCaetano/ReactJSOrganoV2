ec42a350aa4693dc79f889b7b20d0ca3fb8824b9
IcaroCaetano <icaro.caetano@gmail.com>
2023-06-26 18:41:50 -0300

    criacao cards colaborador

---
+++ src/componentes/Colaborador/index.js
@@ -0,0 +1,15 @@
+import './Colaborador.css'
+
+const Colaborador = () => {
+    return (<div className='colaborador'>
+                <div className='cabecalho'>
+                    <img src='https://github.com/IcaroCaetano.png' alt='Ícaro Caetano'/>
+                </div>
+                <div className='rodape'>
+                    <h4>Icaro Caetano</h4>
+                    <h5>Estudante</h5>
+                </div>
+            </div>)
+}
+
+export default Colaborador
